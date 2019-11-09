72ac087d9a93341e2a533277648d23c79be9d958
shivshankar <shejwal1shivshankar@gmail.com>
2019-11-09 21:28:24 +0530

    lesson 1 solved

--- src/helloworld.js
+++ src/helloworld.js
@@ -1,3 +1,8 @@
 module.exports.handler = (evt, ctx, done) => {
-  
+    done(null, evt);
+}
+module.exports.newhello = (evt, ctx, done) => {
+    done(null, {
+        "msg": "new handker try"
+    });
 }
